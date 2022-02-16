1abfe60b874dd65fe67dc33617bb7ed12ac9a11e
Aleksej_Kazakov <mralex1972@mail.ru>
2022-02-16 23:03:01 +0300

    добавлена задача 17

--- seminar1-2.py
+++ seminar1-2.py
@@ -174,6 +174,26 @@
 #    Найти произведение элементов на указанных позициях.
 #    Позиции хранятся в файле file.txt в одной строке одно число.
 #
+# n = int(input('Введите N '))
+# list = []
+# for i in range(2*n + 1):
+#     list.append(i - n)
+# print(list)
+# data = open('file.txt', 'a')
+# import random
+# # Записываем в файл случайное количество случайных значений индексов
+# # элементов списка:
+# for i in range(2, random.randint(3, 2*n + 1)):
+#     num = random.randint(0, 2*n)
+#     data.writelines(f'{num}\n')
+# data.close()
+# data = open('file.txt', 'r')
+# p = 1
+# for line in data:
+#     index = int(line)
+#     p *= list[index]
+# print(f'Произведение элементов равно {p}')
+# data.close()
 
 # 18.	Реализовать алгоритм перемешивания списка.
 #
